2986171a20f72e38616d0bc1e18fe1e2459949b7
QRUMN <quorumnova@gmail.com>
2025-01-02 15:38:48 -0500

    Initial commit: Basic React + TypeScript + Vite setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-center py-8">
+          Welcome to Grinnage Ex
+        </h1>
+      </div>
+    </Router>
+  )
+}
+
+export default App
